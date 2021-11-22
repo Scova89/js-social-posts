042d06fb6b384928410a4db6ca4b107407efbb5b
Francesco <scovascova@gmail.com>
2021-11-22 17:07:23 +0100

    inserimento likes

--- main.js
+++ main.js
@@ -57,25 +57,27 @@ const posts = [
 ];
 
 
+
 let items ='';
 
 for(let i = 0; i < posts.length; i++){
+    
     items += `
     <div class="post">
     <div class="post__header">
                 <div class="post-meta">                    
                     <div class="post-meta__icon">
-                        <img class="profile-pic" src="${posts[i].media}" alt="${posts[i].name}">                    
+                        <img class="profile-pic" src="${posts[i].media}" alt="${posts[i].author.name}">                    
                     </div>
                     <div class="post-meta__data">
-                        <div class="post-meta__author">${posts[i].name}</div>
-                        <div class="post-meta__time">4 mesi fa</div>
+                        <div class="post-meta__author">${posts[i].author.name}</div>
+                        <div class="post-meta__time">${posts[i].created}</div>
                     </div>                    
                 </div>
             </div>
-            <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
+            <div class="post__text">${posts[i].content}</div>
             <div class="post__image">
-                <img src="https://unsplash.it/600/300?image=171" alt="">
+                <img src="${posts[i].author.image}" alt="${posts[i].author.name}">
             </div>
             <div class="post__footer">
                 <div class="likes js-likes">
@@ -86,7 +88,7 @@ for(let i = 0; i < posts.length; i++){
                         </a>
                     </div>
                     <div class="likes__counter">
-                        Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${posts[i].likes}</b> persone
                     </div>
                 </div> 
             </div>
